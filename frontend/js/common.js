866b6c10670c838aa092a40d3bce63499d74dc73
mahmed2401-creator <m.ahmed2401@su.edu.eg>
2026-06-25 02:40:28 +0300

    Fix API base URL

--- frontend/js/common.js
+++ frontend/js/common.js
@@ -1,6 +1,6 @@
 // Common UI Utilities for 7amo Store
 
-const API_URL = 'https://7amo-store.vercel.app';
+const API_URL = 'https://7amo-store.vercel.app/api';
 
 async function apiFetch(endpoint, options = {}) {
   const token = safeStorage.get('token');
